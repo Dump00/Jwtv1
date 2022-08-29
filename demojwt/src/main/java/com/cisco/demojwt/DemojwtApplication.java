fcbbe6df1372ae5acb9b4dc98f3a55b2c0a3610a
sovisrushain <sovisrushain@gmail.com>
2022-08-29 22:28:55 +0530

    fix configure datasource isue

--- demojwt/src/main/java/com/cisco/demojwt/DemojwtApplication.java
+++ demojwt/src/main/java/com/cisco/demojwt/DemojwtApplication.java
@@ -2,8 +2,9 @@ package com.cisco.demojwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemojwtApplication {
 
 	public static void main(String[] args) {
